8877fe107f3276af1ce84f24d5d324a8dc1e82a4
flore <florentfougerat08@aol.fr>
2025-01-28 10:13:39 +0100

    fetch_todos

--- VSO Web Flask/VSO_Web_Flask/views.py
+++ VSO Web Flask/VSO_Web_Flask/views.py
@@ -3,8 +3,11 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template, jsonify
 from VSO_Web_Flask import app
+from .Tp.Exercice1.placeholder import *
+
+
 
 @app.route('/')
 @app.route('/home')
@@ -21,3 +24,13 @@ def weather():
         'weather.html',
         title='Weather Page',
     ) 
+
+@app.route('/fetch_todos', methods=['GET'])
+def fetch_todos():
+    """
+    Cette route récupère les tâches depuis l'API JSONPlaceholder et les stocke dans une liste.
+    """
+    response = requests.get(URL)
+    todos = response.json()
+    save_to_csv(todos)
+    return jsonify(todos)
