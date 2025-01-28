6f72b58033f0c1b5f30099a86e3bf551e8a7e794
duvictorg <dugleux.victor@gmail.com>
2025-01-28 10:14:39 +0100

    weather

--- VSO Web Flask/VSO_Web_Flask/views.py
+++ VSO Web Flask/VSO_Web_Flask/views.py
@@ -2,7 +2,8 @@
 Routes and views for the flask application.
 """
 
-from datetime import datetime
+import requests
+from markupsafe import escape
 from flask import render_template
 from VSO_Web_Flask import app
 
@@ -15,8 +16,9 @@ def home():
         title='Home Page',
     )
 
-@app.route('/weather')
+@app.route('/weather/<Ville>')
 def weather():
+    URL = 'https://api.openweathermap.org/data/2.5/weather?q=' + str(Ville) + '&limit=1&appid=1446ca8783c1ae2790262ec9c1510b9c'
     return render_template(
         'weather.html',
         title='Weather Page',
