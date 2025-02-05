806ed9492881b256200f2d1279c38faa5dba278c
duvictorg <dugleux.victor@gmail.com>
2025-02-05 12:36:28 +0100

    admin list students

--- VSO Web Flask/VSO_Web_Flask/views/admin_views.py
+++ VSO Web Flask/VSO_Web_Flask/views/admin_views.py
@@ -1,3 +1,4 @@
+from re import M
 from flask import Blueprint, render_template, request, session, redirect, url_for
 from VSO_Web_Flask.controller import AuthenticationController
 
@@ -16,10 +17,14 @@ class AdminViews:
                 return redirect(url_for("auth_bp.login"))
             return render_template("admin.html", admin=admin)
 
-        @self.admin_bp.route("/list")
+        @self.admin_bp.route("/list/students")
         def list_grades():
-            grades = self.controller.list_grades()
-            return render_template("admin.html", grades=grades)
+            students = self.controller.list_student()
+            Noms = [D["Nom"] for D in students]
+            Prenoms = [D["Prenom"] for D in students]
+            student_ids = [D["id"] for D in students]
+            Mails = [D["Mail"] for D in students]
+            return render_template("admin_students.html",Noms=Noms,Prenoms=Prenoms,student_ids=student_ids,Mails=Mails)
 
         @self.admin_bp.route("/", methods=["GET", "POST"])
         @self.admin_bp.route("/register", methods=["GET", "POST"])
