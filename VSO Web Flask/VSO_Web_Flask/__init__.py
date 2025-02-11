611b54f3b385e917cd56106cd582857264d62785
duvictorg <dugleux.victor@gmail.com>
2025-02-11 17:25:09 +0100

    correction csrf token

--- VSO Web Flask/VSO_Web_Flask/__init__.py
+++ VSO Web Flask/VSO_Web_Flask/__init__.py
@@ -6,14 +6,23 @@ from VSO_Web_Flask.views.auth import AuthenticationViews
 from VSO_Web_Flask.views.student_views import StudentViews
 from VSO_Web_Flask.views.teacher_views import TeacherViews
 from VSO_Web_Flask.views.admin_views import AdminViews
+from datetime import timedelta
 
 load_dotenv()
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
+    app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(hours=1)
+    app.config["SESSION_REFRESH_EACH_REQUEST"] = True
     csrf = CSRFProtect()
     csrf.init_app(app)
+
+    @app.before_request
+    def set_csrf_token():
+        """ S'assurer que le jeton CSRF est bien dans la session """
+        if "_csrf_token" not in session:
+            session["_csrf_token"] = generate_csrf()
 
     student_views = StudentViews()
     teacher_views = TeacherViews()
