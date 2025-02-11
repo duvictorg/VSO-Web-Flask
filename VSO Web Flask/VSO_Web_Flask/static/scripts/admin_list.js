7671d26c22b4dcbab73db03e2e5161c90f35ac0a
flore <florentfougerat08@aol.fr>
2025-02-11 12:29:23 +0100

    modif admin list

--- VSO Web Flask/VSO_Web_Flask/static/scripts/admin_list.js
+++ VSO Web Flask/VSO_Web_Flask/static/scripts/admin_list.js
@@ -1,1 +1,11 @@
-document.addEventListener()
+document.addEventListener("DOMContentLoaded", () => {
+    const rows = document.querySelectorAll("tr[data-href]")
+    console.log(rows);
+
+
+    rows.forEach(row => {
+        rows.addEventListener("click", () => {
+            windows.location.href = row.dataset.href;
+        });
+    });
+});
