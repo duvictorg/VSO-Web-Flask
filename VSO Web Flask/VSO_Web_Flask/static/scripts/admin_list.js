33e599e9dd351c7a4503476fd06eea2f029ab2af
flore <florentfougerat08@aol.fr>
2025-02-11 18:45:37 +0100

    modif admin

--- VSO Web Flask/VSO_Web_Flask/static/scripts/admin_list.js
+++ VSO Web Flask/VSO_Web_Flask/static/scripts/admin_list.js
@@ -4,30 +4,33 @@ document.addEventListener('DOMContentLoaded', function () {
     const matiereField = document.querySelector(".matiere-field");
     const classeFields = document.querySelector(".classe-fields");
     const addBtn = document.querySelector('#add');
+    const editBtn = document.querySelector('#edit');
     const deleteBtn = document.querySelector('#delete');
     const addForm = document.querySelector('#add-form');
     const deleteForm = document.querySelector('#delete-form');
     const eleveselect = document.querySelector('#select-eleve');
     const teacherselect = document.querySelector('#select-prof');
 
-
     Prof_btn.addEventListener('click', () => {
-        matiereField.style.display = "block";
-        classeFields.style.display = "none";
-        eleveselect.style.display = "none";
+        matiereField.style.display = "none";
+        classeFields.style.display = "block";
         teacherselect.style.display = "block";
     });
 
     eleve_btn.addEventListener('click', () => {
-        matiereField.style.display = "none";
-        classeFields.style.display = "flex";
-        eleveselect.style.display = "block";
+        matiereField.style.display = "flex";
+        classeFields.style.display = "none";
         teacherselect.style.display = "none";
     });
 
     addBtn.addEventListener('click', function () {
         addForm.style.display = 'block';
         deleteForm.style.display = 'none';
+    });
+
+    editBtn.addEventListener('click', function () {
+        addForm.style.display = 'none';
+        deleteForm.style.display = 'block';
     });
 
     deleteBtn.addEventListener('click', function () {
